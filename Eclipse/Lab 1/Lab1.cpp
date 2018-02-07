fb566418b718c65865f6abc47bfec6af98b480aa
pcmean <wwh727476@gmail.com>
2018-02-07 09:41:22 -0800

    2/7/2018

---
+++ Eclipse/Lab 1/Lab1.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Lab 1.cpp
+// Author      : Walter Hicks
+// Version     : 1
+// Copyright   : This code is for me and me allone
+// Description : Lab 1
+//============================================================================
+
+#include "Lab1.h"
+
+Lab1::Lab1() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Lab1::~Lab1() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Eclipse/Lab 1/Lab1.h
@@ -0,0 +1,17 @@
+/*
+ * Lab1.h
+ *
+ *  Created on: Feb 7, 2018
+ *      Author: wwh72
+ */
+
+#ifndef LAB1_H_
+#define LAB1_H_
+
+class Lab1 {
+public:
+	Lab1();
+	virtual ~Lab1();
+};
+
+#endif /* LAB1_H_ */
